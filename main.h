bde005bbfe7cba033ee1f8a84b7fb8375865f2e9
«loubassouM» <loubassoumurphy@gmail.com>
2025-03-24 13:42:16 +0000

    Creation du fichier printf.c

--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 int _putchar(char c);
 int _strlen_recursion(char *s);
 int _printf(const char *format, ...);
+<<<<<<< HEAD
 void print_numbers(void);
 void print_most_numbers(void);
 void print_line(int n);
@@ -27,5 +28,13 @@ void print_dog(struct dog *d);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+=======
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_dog(struct dog *d);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+>>>>>>> 40902f1 (Ajout prototypes main.h)
 
 #endif
